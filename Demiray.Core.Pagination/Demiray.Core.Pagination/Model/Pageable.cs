6e75dab3792dbe307b230d43f490eced3ccd679f
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2021-09-30 20:25:20 +0300

    fea: add pageable.of instance

--- Demiray.Core.Pagination/Demiray.Core.Pagination/Model/Pageable.cs
+++ Demiray.Core.Pagination/Demiray.Core.Pagination/Model/Pageable.cs
@@ -5,5 +5,23 @@
         public int? PageNumber { get; set; }
 
         public int? PageSize { get; set; }
+
+        /// <summary>
+        /// This function creates a pageable instance by given pageNumber and pageSize
+        /// - If you provide only page size it takes default 0 for the page number
+        /// - If you provide only page number it takes default 10 for the page size
+        /// - If you do not provide any input it takes defaults of above
+        /// </summary>
+        /// <param name="pageNumber"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static Pageable Of(int? pageNumber = 0, int? pageSize = 10)
+        {
+            return new Pageable()
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+        }
     }
 }
